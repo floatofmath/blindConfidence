02473b51d4d529d405323e4196d7271e4aff0908
Florian Klinglmueller <float@lefant.net>
2015-07-29 10:06:59 +0200

    added simulation data and corresponding documentation to package

---
+++ R/data.R
@@ -0,0 +1,58 @@
+#' Simulation results from "Estimation after blinded interim analyses"
+#'
+#' @template datades
+#' 
+#' @format A \code{data.frame}, each line corresponds to the outcome from
+#' one simulation run with $10^6$ simulated trials.
+#' @name gridsim
+NULL
+
+#' Maximum Bias Simulations "Estimation after blinded interim analyses"
+#'
+#' @template datades
+#'
+#' @format A \code{data.frame}, each line corresponds to the outcome from
+#' one simulation run with $10^6$ simulated trials.
+#' @name maxsim
+NULL
+
+#' Maximum Bias Re-Simulations of mean estimates 
+#' "Estimation after blinded interim analyses" 
+#'
+#' @template datades
+#'
+#' @format A \code{data.frame}, each line corresponds to the outcome from
+#' one simulation run with $10^7$ simulated trials.
+#' @name maxmeansim
+NULL
+
+#' Maximum Bias Re-Simulations of mean estimates 
+#' "Estimation after blinded interim analyses"
+#'
+#' @template datades
+#'
+#' @format A \code{data.frame}, each line corresponds to the outcome from
+#' one simulation run with $10^7$ simulated trials.
+#' @name maxvariancesim
+NULL
+
+#' Maximum Bias Re-Simulations of mean estimates 
+#' "Estimation after blinded interim analyses"
+#'
+#' @template datades
+#'
+#' @format A \code{data.frame}, each line corresponds to the outcome from
+#' one simulation run with $10^7$ simulated trials.
+#' @name maxcoveragesim
+NULL
+
+
+#' Case study simulations "Estimation after blinded interim analyses"
+#'
+#' @template datades
+#'
+#' @format A \code{data.frame}, each line corresponds to the outcome from
+#' one simulation run with $10^6$ simulated trials.
+#' @name casesim
+NULL
+
