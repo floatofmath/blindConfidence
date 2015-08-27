9c153be544885e16d679daf43b295175356c11f8
Florian Klinglmueller <float@lefant.net>
2015-08-27 10:04:07 +0200

    added function that runs coverage simulations

---
+++ R/coveragesim.R
@@ -0,0 +1,44 @@
+##' run coverage sims 
+##'
+##' @title coverage sim
+##' @param search number of simulation runs for the wide search
+##' @param resim number of simulation runs for the resimulation of found maxima
+##' @param use_mclapply2 use mclapply2 in simulations
+##' @return dataframe with simulation results
+##' @author float
+##' @export
+coverage_sim <- function(search=10^4,resim=10^6,use_mclapply2=TRUE){
+## Seperate file to run coverage simulations as they tend to crash the rstudio server
+    library(parallel)
+    ## devtools::install_github('floatofmath/blindConfidence')
+    library(blindConfidence)
+    library(bt88.03.704)
+    library(ggplot2)
+    library(reshape2)
+    library(plyr)
+
+    data(maxsim)
+    maxsim$ylow <- 0
+
+    maxcoverageopt1 <- select_results(maxsim,c('total.prob','upper.prob','uc.total.prob','uc.upper.prob'),base_columns=c('n1','tn1','delta','sigma','d','s','total.prob','upper.prob'),functional=which.max)
+
+    G2coverage <- add_epsilon(maxcoverageopt1,.05,.01,c('delta','sigma'))
+##plot_grid(maxsim,maxcoverageopt1,G2coverage,'sigma')
+    cat("Run search\n")
+    options(mc.cores=min(20,detectCores()-1))
+    maxcoveragesim2 <- simulate_batch(G2coverage,search,use_mclapply2)
+    gc()
+
+## reshape and resim
+    maxcoverageopt2 <- select_results(maxcoveragesim2,c('total.prob','upper.prob','uc.total.prob','uc.upper.prob'),base_columns=c('n1','tn1','delta','sigma','d','s'),functional=which.max)
+    options(mc.cores=min(16,detectCores()-1))
+    cat("Run resimulation\n")
+    maxcoveragesim3 <- simulate_batch(maxcoverageopt2,resim,use_mclapply2)
+    gc()
+
+
+    fname <- paste('resim_coverage',Sys.info()['nodename'],'_',format(Sys.time(),"%y%m%d-%H%M"),'.Rd',sep='')
+    save(maxcoveragesim3,file=fname)
+    cat("Saved results to:",fname,"\n")
+    maxcoveragesim3
+}
