b06d20fcca474b394a6575895546769f87506e31
Florian Klinglmueller <float@lefant.net>
2015-08-31 09:48:34 +0200

    further tweaks on maximum coverage simulations

--- R/coveragesim.R
+++ R/coveragesim.R
@@ -5,10 +5,12 @@
 ##' @param resim number of simulation runs for the resimulation of found maxima
 ##' @param use_mclapply2 use mclapply2 in simulations
 ##' @param mem_factor number of cores that can handle 10^7 simulation runs, will be used to automatically determine the number of cores 
+##' @param epsilon width of parameter grid around initial values
+##' @param by fineness of grid around initial values
 ##' @return dataframe with simulation results
 ##' @author float
 ##' @export
-coverage_sim <- function(search=10^4,resim=10^6,use_mclapply2=TRUE,mem_factor=16){
+coverage_sim <- function(search=10^4,resim=10^6,use_mclapply2=TRUE,mem_factor=16,epsilon=.18,by=.06){
 ## Seperate file to run coverage simulations as they tend to crash the rstudio server
     library(parallel)
     ## devtools::install_github('floatofmath/blindConfidence')
@@ -22,7 +24,7 @@ coverage_sim <- function(search=10^4,resim=10^6,use_mclapply2=TRUE,mem_factor=16
 
     maxcoverageopt1 <- select_results(maxsim,c('total.prob','upper.prob','uc.total.prob','uc.upper.prob'),base_columns=c('n1','tn1','delta','sigma','d','s','total.prob','upper.prob'),functional=which.max)
 
-    G2coverage <- add_epsilon(maxcoverageopt1,.05,.01,c('delta','sigma'))
+    G2coverage <- add_epsilon(maxcoverageopt1,epsilon,by,c('delta','sigma'))
 ##plot_grid(maxsim,maxcoverageopt1,G2coverage,'sigma')
     cat("Run search\n")
     s.cores <- floor(mem_factor* 10^7/search)
@@ -44,3 +46,4 @@ coverage_sim <- function(search=10^4,resim=10^6,use_mclapply2=TRUE,mem_factor=16
     cat("Saved results to:",fname,"\n")
     maxcoveragesim3
 }
+
